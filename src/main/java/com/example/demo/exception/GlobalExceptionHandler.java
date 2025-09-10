a76903cf2f3fca71100b5a667b840027d6a50b40
DeepaJava-ct <deepavetrivel4@gmail.com>
2025-09-10 15:35:45 +0530

    Merged the three modules

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.example.demo.exception;
 
 import java.time.LocalDateTime;
- renu-feature01-local
+ //renu-feature01-local
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,49 +47,50 @@ public class GlobalExceptionHandler {
 	        );
 	        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
-
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-	
-	@ExceptionHandler(VehicleNotFoundException.class)
-	public ResponseEntity<Map<String , Object>> handleVehicleNootFound(VehicleNotFoundException ex){
-		return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
-		
-	}
-	
-	@ExceptionHandler(TripNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleTripNotFound(TripNotFoundException ex) {
-        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(ShipmentNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleShipmentNotFound(ShipmentNotFoundException ex) {
-        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(Exception.class) // fallback for any other exception
-    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
-        return buildResponse("Internal Server Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private ResponseEntity<Map<String, Object>> buildResponse(String message, HttpStatus status) {
-        Map<String, Object> error = new HashMap<>();
-        error.put("timestamp", LocalDateTime.now());
-        error.put("status", status.value());
-        error.put("error", status.getReasonPhrase());
-        error.put("message", message);
-
-        return new ResponseEntity<>(error, status);
-    }
- master
-
 }
+
+
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//	
+//	@ExceptionHandler(VehicleNotFoundException.class)
+//	public ResponseEntity<Map<String , Object>> handleVehicleNootFound(VehicleNotFoundException ex){
+//		return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+//		
+//	}
+//	
+//	@ExceptionHandler(TripNotFoundException.class)
+//    public ResponseEntity<Map<String, Object>> handleTripNotFound(TripNotFoundException ex) {
+//        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(ShipmentNotFoundException.class)
+//    public ResponseEntity<Map<String, Object>> handleShipmentNotFound(ShipmentNotFoundException ex) {
+//        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(Exception.class) // fallback for any other exception
+//    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+//        return buildResponse("Internal Server Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//
+//    private ResponseEntity<Map<String, Object>> buildResponse(String message, HttpStatus status) {
+//        Map<String, Object> error = new HashMap<>();
+//        error.put("timestamp", LocalDateTime.now());
+//        error.put("status", status.value());
+//        error.put("error", status.getReasonPhrase());
+//        error.put("message", message);
+//
+//        return new ResponseEntity<>(error, status);
+//    }
+// 
+//
+//}
